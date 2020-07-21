06e89d73416e39d12b4b5dbe4c2c07a370b17807
alexisfretez <alexisfretez@gmail.com>
2020-07-20 23:38:37 -0400

    Avatar

--- SistemaDeposito/application/views/layouts/header.php
+++ SistemaDeposito/application/views/layouts/header.php
@@ -113,12 +113,12 @@
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="<?php echo base_url() ?>assets/template/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                                <img src="<?php echo base_url() ?>assets/template/dist/img/avatar04.png" class="user-image" alt="User Image">
                                 <span class="hidden-xs"><?php echo $this->session->userdata("nombre") ?></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <li class="user-header">
-                                    <img src="<?php echo base_url() ?>assets/template/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" text-align="center">
+                                    <img src="<?php echo base_url() ?>assets/template/dist/img/avatar04.png" class="img-circle" alt="User Image" text-align="center">
                                     <p>
                                         <span text-align="center" class="hidden-xs"><?php echo $this->session->userdata("nombre") ?></span>
                                     </p>
