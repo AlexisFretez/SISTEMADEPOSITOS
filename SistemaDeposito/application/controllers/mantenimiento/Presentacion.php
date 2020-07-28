929979fa35670f48c39896806a4e1ccb5f563059
alexisfretez <alexisfretez@gmail.com>
2020-07-28 19:48:54 -0400

    Presentacion ok

--- SistemaDeposito/application/controllers/mantenimiento/Presentacion.php
+++ SistemaDeposito/application/controllers/mantenimiento/Presentacion.php
@@ -1,10 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Presentacion extends CI_Controller {
+class Presentacion extends CI_Controller
+{
 
 	private $permisos;
-	public function __construct(){
+	public function __construct()
+	{
 		parent::__construct();
 		$this->permisos = $this->backend_lib->control();
 		$this->load->model("Presentacion_model");
@@ -15,16 +17,16 @@ class Presentacion extends CI_Controller {
 	{
 		$data  = array(
 			'permisos' => $this->permisos,
-			'presentaciones' => $this->Presentacion_model->getPresentaciones(), 
+			'presentaciones' => $this->Presentacion_model->getPresentaciones(),
 		);
 		$this->load->view("layouts/header");
 		$this->load->view("layouts/aside");
-		$this->load->view("admin/presentacion/list",$data);
+		$this->load->view("admin/presentacion/list", $data);
 		$this->load->view("layouts/footer");
-
 	}
 
-	public function add(){
+	public function add()
+	{
 
 		$this->load->view("layouts/header");
 		$this->load->view("layouts/aside");
@@ -32,94 +34,91 @@ class Presentacion extends CI_Controller {
 		$this->load->view("layouts/footer");
 	}
 
-	public function store(){
+	public function store()
+	{
 
 		$nombre = $this->input->post("nombre");
-	//	$descripcion = $this->input->post("descripcion");
+		//	$descripcion = $this->input->post("descripcion");
 
-		$this->form_validation->set_rules("nombre","Nombre","required|is_unique[presentacion.nombre]");
+		$this->form_validation->set_rules("nombre", "Nombre", "required|is_unique[presentacion.nombre]");
 
-		if ($this->form_validation->run()==TRUE) {
+		if ($this->form_validation->run() == TRUE) {
 
 			$data  = array(
-				'nombre' => $nombre, 
+				'nombre' => $nombre,
 				'estado' => "1"
 			);
 
 			if ($this->Presentacion_model->save($data)) {
-				redirect(base_url()."mantenimiento/presentacion");
+				redirect(base_url() . "mantenimiento/presentacion");
+			} else {
+				$this->session->set_flashdata("error", "No se pudo guardar la informacion");
+				redirect(base_url() . "mantenimiento/presentacion/add");
 			}
-			else{
-				$this->session->set_flashdata("error","No se pudo guardar la informacion");
-				redirect(base_url()."mantenimiento/presentacion/add");
-			}
-		}
-		else{
+		} else {
 			/*redirect(base_url()."mantenimiento/marcas/add");*/
 			$this->add();
 		}
-
-		
 	}
 
-	public function edit($id){
+	public function edit($id)
+	{
 		$data  = array(
-			'presentacion' => $this->Presentacion_model->getPresentacion($id), 
+			'presentacion' => $this->Presentacion_model->getPresentacion($id),
 		);
 		$this->load->view("layouts/header");
 		$this->load->view("layouts/aside");
-		$this->load->view("admin/presentacion/edit",$data);
+		$this->load->view("admin/presentacion/edit", $data);
 		$this->load->view("layouts/footer");
 	}
 
-	public function update(){
-		$idMarca = $this->input->post("idMarca");
+	public function update()
+	{
+		$idPresentacion = $this->input->post("idPresentacion");
 		$nombre = $this->input->post("nombre");
 		//$descripcion = $this->input->post("descripcion");
 
-		$marcaactual = $this->Presentacion_model->getPresentacion($idMarca);
+		$marcaactual = $this->Presentacion_model->getPresentacion($idPresentacion);
 
 		if ($nombre == $marcaactual->nombre) {
 			$is_unique = "";
-		}else{
+		} else {
 			$is_unique = "|is_unique[presentacion.nombre]";
-
 		}
 
 
-		$this->form_validation->set_rules("nombre","Nombre","required".$is_unique);
-		if ($this->form_validation->run()==TRUE) {
+		$this->form_validation->set_rules("nombre", "Nombre", "required" . $is_unique);
+		if ($this->form_validation->run() == TRUE) {
 			$data = array(
-				'nombre' => $nombre, 
-				'descripcion' => $descripcion,
+				'nombre' => $nombre,
+				//'descripcion' => $descripcion,
 			);
 
-			if ($this->Presentacion_model->update($idMarca,$data)) {
-				redirect(base_url()."mantenimiento/presentacion");
+			if ($this->Presentacion_model->update($idPresentacion, $data)) {
+				redirect(base_url() . "mantenimiento/presentacion");
+			} else {
+				$this->session->set_flashdata("error", "No se pudo actualizar la informacion");
+				redirect(base_url() . "mantenimiento/presentacion/edit/" . $idPresentacion);
 			}
-			else{
-				$this->session->set_flashdata("error","No se pudo actualizar la informacion");
-				redirect(base_url()."mantenimiento/presentacion/edit/".$idMarca);
-			}
-		}else{
-			$this->edit($idMarca);
+		} else {
+			$this->edit($idPresentacion);
 		}
-
-		
 	}
 
-	public function view($id){
+	public function view($id)
+	{
 		$data  = array(
-			'presentacion' => $this->Presentacion_model->getPresentacion($id), 
+			'presentacion' => $this->Presentacion_model->getPresentacion($id),
 		);
-		$this->load->view("admin/presentacion/view",$data);
+		$this->load->view("admin/presentacion/view", $data);
 	}
 
-	public function delete($id){
+	public function delete($id)
+	{
 		$data  = array(
-			'estado' => "0", 
+			'estado' => "0",
 		);
-		$this->Presentacion_model->update($id,$data);
+		$this->Presentacion_model->update($id, $data);
 		echo "mantenimiento/presentacion";
 	}
 }
